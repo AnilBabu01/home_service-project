8063746086435791521228045d4dba6ccf03b8c7
AnilBabu01 <anilbabu3245@gmail.com>
2026-02-21 09:07:57 +0530

    working on dashboard

--- adminNo/src/pages/Dashboard/ECommerce.tsx
+++ adminNo/src/pages/Dashboard/ECommerce.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import CardDataStats from '../../components/CardDataStats';
-import ChartOne from '../../components/Charts/ChartOne';
-import ChartThree from '../../components/Charts/ChartThree';
-import ChartTwo from '../../components/Charts/ChartTwo';
-import ChatCard from '../../components/Chat/ChatCard';
-import MapOne from '../../components/Maps/MapOne';
-import TableOne from '../../components/Tables/TableOne';
+// import ChartOne from '../../components/Charts/ChartOne';
+// import ChartThree from '../../components/Charts/ChartThree';
+// import ChartTwo from '../../components/Charts/ChartTwo';
+// import ChatCard from '../../components/Chat/ChatCard';
+// import MapOne from '../../components/Maps/MapOne';
+// import TableOne from '../../components/Tables/TableOne';
 import DefaultLayout from '../../layout/DefaultLayout';
 import { useGetDashboardDataQuery } from '../../redux/api';
 const ECommerce: React.FC = () => {
